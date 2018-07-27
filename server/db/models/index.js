bc2c31af869c4e9e3ab8c757941ef2bc4de7b1cb
Tiny-Sheep <lsadoun@mail.bradley.edu>
2018-07-27 12:44:42 -0500

    initial commit

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,5 @@
 const User = require('./user')
+const Vote = require('./vote')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -6,6 +7,9 @@ const User = require('./user')
  *
  *    BlogPost.belongsTo(User)
  */
+Vote.belongsTo(User)
+User.hasOne(Vote)
+
 
 /**
  * We'll export all of our models here, so that any time a module needs a model,
@@ -14,5 +18,7 @@ const User = require('./user')
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User
+  User,
+  Vote
 }
+
