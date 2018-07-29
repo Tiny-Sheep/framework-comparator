0780b703630c4c9febb647fbe3e1bd9666cfcfc9
Tiny-Sheep <lsadoun@mail.bradley.edu>
2018-07-29 11:51:54 -0500

    nearly completed the gitHub api file

--- server/api/github-api-check.js
+++ server/api/github-api-check.js
@@ -1,36 +1,81 @@
 const axios = require('axios')
 const { Framework } = require('../db/models')
-
+// import { GITHUB_TOKEN } from '../../secrets'
 
 const lastWeek = new Date(Date.now() - (86400000 * 7))
 const lastWeekISO = lastWeek.toISOString().split("T")[0]
 
 const frameworkLinks = ['facebook/react', 'angular/angular.js', 'vuejs/vue', 'emberjs/ember.js']
+const frameworkNames = ['react', 'angular.js', 'vuejs', 'ember.js']
+
+
+const updateFramework = {}
+const updateFrameworkArr = []
 
 const getGitHubData = async () => {
 
-  const updateFramework = {}
 
-  const updateFrameworkArr = []
-
-  frameworkLinks.map((frameworkLink) => {
-    axios.get(`https://api.github.com/repos/${frameworkLink}`, {
-      headers: {
-        accept: 'application/vnd.github.v3+json'
-      }
-    }).then(async (response) => {
-
-      updateFramework.name = response.data.name
-      updateFramework.watchers = response.data.subscribers_count
-      updateFramework.issues = response.data.open_issues
-      updateFrameworkArr.push(updateFramework)
-
-
-    })
-
+  const arrOfWatcherAndIssueProms = frameworkLinks.map((frameworkLink) => {
+    try {
+      return axios.get(`https://api.github.com/repos/${frameworkLink}`, {
+        headers: {
+          accept: 'application/vnd.github.v3+json',
+          Authorization: "32fc2848180291630d829b96c069954480cad514"
+        }
+      })
+    } catch (err) {
+      console.error(err)
+    }
   })
 
-  console.log("this array should be filled", updateFrameworkArr)
+  const arrOfPushesProms = frameworkNames.map((frameworkName) => {
+    try {
+      return axios.get(`https://api.github.com/search/repositories?q=${frameworkName}+in:name,description,readme+pushed:>${lastWeekISO}`, {
+        headers: {
+          accept: 'application/vnd.github.v3+json',
+          Authorization: "32fc2848180291630d829b96c069954480cad514"
+        }
+      })
+
+    } catch (err) {
+      console.error(err)
+    }
+  })
+
+
+  const resolvedPushPromises = await Promise.all(arrOfPushesProms)
+  const resolvedWatcherAndIssuePromises = await Promise.all(arrOfWatcherAndIssueProms)
+  const pushesData = resolvedPushPromises.map((push) => {
+    return push.data.total_count
+  })
+
+  const updateFrameWorks = resolvedWatcherAndIssuePromises.map((response, i) => {
+
+    return { issues: response.data.open_issues, watchers: response.data.subscribers_count, name: response.data.name, pushes: pushesData[i] }
+  })
+
+  const updateFrameworkPromises = updateFrameWorks.map(frameworkData => {
+    console.log("what is this frameworkData", frameworkData)
+    return Framework.update(frameworkData, {
+      where: {
+        name: frameworkData.name
+      }
+    })
+  })
+
+  const updatedFrameWorks = Promise.all(updateFrameworkPromises)
+
+  console.log("my updated frameworks", updatedFrameWorks)
+
+  // console.log("resolved push promises", resolvedPushPromises)
+
+
+
+  // next loop through this data
+
+
+
+
 
   // console.log("what is in this array of issue?", arrayOfIssues)
 
@@ -67,6 +112,6 @@ const getGitHubData = async () => {
 }
 
 
-
 getGitHubData()
+
 
